07d85f56187f11cb3d23e5bce8ff58452d52b0b9
sunakshi09 <sunakshithapar@yahoo.com>
2023-10-22 05:09:50 +0000

    values

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-`
+class Currency:
+
+currencies = {
+    'CHF': 0.930023,  #swiss franc 
+    'CAD': 1.264553,  #canadian dollar
+    'GBP': 0.737414,  #british pound
+    'JPY': 111.019919,  #japanese yen
+    'EUR': 0.862361,  #euro
+    'USD': 1.0  #us dollar
+}
+
+def __init__(self, value, unit="USD"):
+    self.value = value
+    self.unit = unit
